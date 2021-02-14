300feb3e3c1a73d8511137e6e787b01ba304544e
Gert van den Buijs <gert.vandenbuijs@tactics.be>
2021-02-14 10:45:40 +0100

    Phpstan max level and add ignore errors

--- phpstan.php
+++ phpstan.php
@@ -22,6 +22,13 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PARALLEL_MAX_PROCESSES, 6);
     $parameters->set(Option::REPORT_UNMATCHED_IGNORED_ERRORS, false);
 
+    $parameters->set(
+        OPTION::IGNORE_ERRORS,
+        [
+            "#Call to an undefined method Symfony\\Component\\Config\\Definition\\Builder\\NodeDefinition\:\:children\(\)#"
+        ]
+    );
+
     $containerConfigurator->import(__DIR__ . '/vendor/phpstan/phpstan/conf/bleedingEdge.neon');
     $containerConfigurator->import(__DIR__ . '/vendor/symplify/phpstan-extensions/config/config.neon');
 
